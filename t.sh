ff0f85caf31a1653967abb8963844bd53961bb49
Allen Guo <guosonglin001@gmail.com>
2023-02-10 15:19:09 +0000

    add stubgen

--- t.sh
+++ t.sh
@@ -3,3 +3,9 @@ pip install -vvv -e .
 # cmake build debug library
 
 objdump --syms torchdynamo/_eval_frame.so | grep debug
+
+pip install mypy
+
+stubgen \
+  -m torchdynamo._eval_frame \
+  -o $PWD
