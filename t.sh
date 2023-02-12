511ca44b6c2ecd83700991ce8e2ee300ad5fe31e
Allen Guo <guosonglin001@gmail.com>
2023-02-12 14:21:10 +0000

    add debug config

---
+++ t.sh
@@ -0,0 +1,12 @@
+pip install -vvv -e .
+
+# cmake build debug library
+
+objdump --syms torchdynamo/_eval_frame.so | grep debug
+
+pip install mypy
+
+stubgen \
+  -m torchdynamo._eval_frame \
+  -m torchdynamo._guards \
+  -o $PWD
