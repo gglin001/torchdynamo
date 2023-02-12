a61c7146598991f91555bfb782fca604cda176d3
Allen Guo <guosonglin001@gmail.com>
2023-02-12 14:34:45 +0000

    add debug config 2

--- t.sh
+++ t.sh
@@ -1,12 +1,17 @@
 pip install -vvv -e .
 
 # cmake build debug library
+rm torchdynamo/_eval_frame.cpython*.so
 
 objdump --syms torchdynamo/_eval_frame.so | grep debug
+
+# objdump --syms torchdynamo/_guards.cpython-38-x86_64-linux-gnu.so | grep debug
 
 pip install mypy
 
 stubgen \
   -m torchdynamo._eval_frame \
-  -m torchdynamo._guards \
   -o $PWD
+
+# torchdynamo._guards
+# ImportError: torchdynamo/_guards.so: undefined symbol: THPVariableClass
